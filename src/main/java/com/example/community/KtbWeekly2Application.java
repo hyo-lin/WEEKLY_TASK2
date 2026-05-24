9a7dc1f21a7367e93d0f4ca0e9e7e14f79691716
hyo-lin <lin1137001@gmail.com>
2026-05-24 15:29:12 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/com/example/community/KtbWeekly2Application.java
@@ -0,0 +1,13 @@
+package com.example.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtbWeekly2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KtbWeekly2Application.class, args);
+    }
+
+}
