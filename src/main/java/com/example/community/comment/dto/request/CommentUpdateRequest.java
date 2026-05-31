b28001dbb0da9863b9055d8bc1f9ae5e8404ee2e
hyo-lin <lin1137001@gmail.com>
2026-05-31 19:47:04 +0900

    feat: 댓글 등록 및 수정을 위한 Request DTO 구현

---
+++ src/main/java/com/example/community/comment/dto/request/CommentUpdateRequest.java
@@ -0,0 +1,10 @@
+package com.example.community.comment.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+
+@Getter
+public class CommentUpdateRequest {
+    @NotBlank
+    private String content;
+}
