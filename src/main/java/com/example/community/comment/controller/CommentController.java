de614c71ca04937220f046979544ad18dbd3b076
hyo-lin <lin1137001@gmail.com>
2026-05-31 19:44:45 +0900

    feat: 댓글 CRUD 처리를 위한 CommentController 구현

---
+++ src/main/java/com/example/community/comment/controller/CommentController.java
@@ -0,0 +1,64 @@
+package com.example.community.comment.controller;
+
+import com.example.community.comment.dto.request.CommentCreateRequest;
+import com.example.community.comment.dto.request.CommentUpdateRequest;
+import com.example.community.comment.dto.response.CommentResponse;
+import com.example.community.comment.service.CommentService;
+import com.example.community.global.response.CommonResponse;
+import com.example.community.global.response.StatusCode;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/posts/{postId}/comments")
+public class CommentController {
+
+    private final CommentService commentService;
+
+    // 댓글 등록
+    @PostMapping
+    public ResponseEntity<CommonResponse<CommentResponse>> createComment(
+            @PathVariable Long postId,
+            @RequestAttribute("userId") Long userId,
+            @RequestBody @Valid CommentCreateRequest request
+    ){
+        return ResponseEntity.ok(CommonResponse.success(StatusCode.CREATE_COMMENT_SUCCESS, commentService.createComment(postId, userId, request)));
+
+    }
+    // 댓글 목록 조회
+    @GetMapping
+    public ResponseEntity<CommonResponse<List<CommentResponse>>> getComments(
+            @PathVariable Long postId,
+            @RequestAttribute("userId") Long userId
+    ){
+        return ResponseEntity.ok(CommonResponse.success(StatusCode.GET_COMMENTS_SUCCESS, commentService.getComments(postId)));
+    }
+
+    //댓글 수정
+    @PatchMapping("/{commentId}")
+    public ResponseEntity<CommonResponse<CommentResponse>> updateComment(
+            @PathVariable Long postId,
+            @PathVariable Long commentId,
+            @RequestAttribute("userId") Long userId,
+            @RequestBody @Valid CommentUpdateRequest request
+    ){
+        return ResponseEntity.ok(CommonResponse.success(StatusCode.UPDATE_COMMENT_SUCCESS, commentService.updateComment(commentId, userId, request)));
+    }
+
+    //댓글 삭제
+    @DeleteMapping("/{commentId}")
+    public ResponseEntity<CommonResponse<Void>> deleteComment(
+            @PathVariable Long postId,
+            @PathVariable Long commentId,
+            @RequestAttribute("userId") Long userId
+    ){
+        commentService.deleteComment(commentId,userId);
+        return ResponseEntity.ok(CommonResponse.success(StatusCode.DELETE_COMMENT_SUCCESS, null));
+    }
+
+}
