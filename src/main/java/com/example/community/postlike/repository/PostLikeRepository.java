c9fd703f06b6798432706d5e55e16f16c9347304
hyo-lin <lin1137001@gmail.com>
2026-05-31 19:55:36 +0900

    feat: PostLikeRepository 구현 및 중복 확인 쿼리 메서드 추가

---
+++ src/main/java/com/example/community/postlike/repository/PostLikeRepository.java
@@ -0,0 +1,12 @@
+package com.example.community.postlike.repository;
+
+import com.example.community.postlike.model.PostLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PostLikeRepository extends JpaRepository<PostLike, Long> {
+    Optional<PostLike> findByPostIdAndUserId(Long postId, Long userId);
+    boolean existsByPostIdAndUserId(Long postId, Long userId);
+
+}
