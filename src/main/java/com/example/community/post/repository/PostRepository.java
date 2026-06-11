f05b9238fce8fea5b9987bfd44d6c1ca1fce420c
hyo-lin <lin1137001@gmail.com>
2026-06-11 14:43:38 +0900

    feat: 게시물 제목 키워드 검색 로직 추가

--- src/main/java/com/example/community/post/repository/PostRepository.java
+++ src/main/java/com/example/community/post/repository/PostRepository.java
@@ -2,6 +2,13 @@ package com.example.community.post.repository;
 
 import com.example.community.post.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    @Query("SELECT p FROM Post p WHERE p.title LIKE %:keyword%")
+    List<Post> searchByTitle(@Param("keyword") String keyword);
 }
