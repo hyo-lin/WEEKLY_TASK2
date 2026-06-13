9d842588fdd85087312c45508c9c1c750f68533b
hyo-lin <lin1137001@gmail.com>
2026-06-13 20:54:38 +0900

    refactor: 탈퇴자 제외 이메일 조회로 변경

--- src/main/java/com/example/community/user/repository/UserRepository.java
+++ src/main/java/com/example/community/user/repository/UserRepository.java
@@ -6,7 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Long> {
-    Optional<User> findByEmail(String email);
+    Optional<User> findByEmailAndDeletedAtIsNull(String email);
+
 
     // ️ 이메일 조건문 처리: 이메일이 일치하고 && 아직 탈퇴하지 않은 유저가 있는지 확인
     boolean existsByEmailAndDeletedAtIsNull(String email);
