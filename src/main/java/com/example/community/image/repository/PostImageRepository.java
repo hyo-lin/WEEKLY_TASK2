7448515a68bc1c325be0add49b18ce77ff5f7e21
hyo-lin <lin1137001@gmail.com>
2026-06-13 20:47:43 +0900

    refactor: 불필요한 jpa 제거

--- src/main/java/com/example/community/image/repository/PostImageRepository.java
+++ src/main/java/com/example/community/image/repository/PostImageRepository.java
@@ -2,13 +2,10 @@ package com.example.community.image.repository;
 
 import com.example.community.image.model.PostImage;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
-import java.util.Optional;
 
 public interface PostImageRepository extends JpaRepository<PostImage, Long> {
-    List<PostImage> findByPostIdAndDeletedAtIsNull(Long postId);
-    Optional<PostImage> findByIdAndPostIdAndDeletedAtIsNull(Long id, Long postId);
 
-    Optional<PostImage> findByImageUrlAndPostIsNull(String imageUrl);
+    List<PostImage> findByPostIdAndDeletedAtIsNull(Long postId);
+
 }
