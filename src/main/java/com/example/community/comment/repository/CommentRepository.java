0216f4d2270af0b2cb907fd0e4226a11dee011ea
hyo-lin <lin1137001@gmail.com>
2026-06-21 14:30:39 +0900

    feat: 댓글 목록 조회 시 페이징 적용

--- src/main/java/com/example/community/comment/repository/CommentRepository.java
+++ src/main/java/com/example/community/comment/repository/CommentRepository.java
@@ -1,10 +1,11 @@
 package com.example.community.comment.repository;
 
 import com.example.community.comment.model.Comment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> findAllByPostId(Long postId);
+    Page<Comment> findAllByPostId(Long postId, Pageable pageable);
+
 }
